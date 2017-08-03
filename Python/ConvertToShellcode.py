1e1dd2ddf3641db597e721e405d99b5db9729324
Spencer McIntyre <zeroSteiner@gmail.com>
2017-08-02 20:03:14 -0400

    Change references from RDLL to DLL

--- Python/ConvertToShellcode.py
+++ Python/ConvertToShellcode.py
@@ -6,15 +6,15 @@ __version__ = '1.0'
 def main():
     parser = argparse.ArgumentParser(description='RDI Shellcode Converter', conflict_handler='resolve')
     parser.add_argument('-v', '--version', action='version', version='%(prog)s Version: ' + __version__)
-    parser.add_argument('input_rdll', help='RDLL to convert to shellcode')
+    parser.add_argument('input_dll', help='DLL to convert to shellcode')
     parser.add_argument('-f', '--function-name', dest='function_name', default='SayHello', help='The function to call after DllMain')
     arguments = parser.parse_args()
 
-    input_rdll = arguments.input_rdll
-    output_bin = input_rdll.replace('.dll', '.bin')
+    input_dll = arguments.input_dll
+    output_bin = input_dll.replace('.dll', '.bin')
 
     print('Creating Shellcode: {}'.format(output_bin))
-    dll = open(arguments.input_rdll, 'rb').read()
+    dll = open(arguments.input_dll, 'rb').read()
 
     converted_dll = ConvertToShellcode(dll, HashFunctionName(arguments.function_name))
     with open(output_bin, 'wb') as f:
