ae4a8a620e93bd3b0bb3687482bf1fd2ef6e3da4
Jerome <jerome@wallaby.be>
2017-10-12 14:16:05 +0200

    Python: Non-ASCII character error in pefile.py

--- Python/pefile.py
+++ Python/pefile.py
@@ -4762,7 +4762,7 @@ class PE:
 	# [ Microsoft Portable Executable and Common Object File Format Specification ]
 	# "The alignment factor (in bytes) that is used to align the raw data of sections in
 	#  the image file. The value should be a power of 2 between 512 and 64 K, inclusive.
-	#  The default is 512. If the SectionAlignment is less than the architecture’s page
+	#  The default is 512. If the SectionAlignment is less than the architecture's page
 	#  size, then FileAlignment must match SectionAlignment."
 	#
 	# The following is a hard-coded constant if the Windows loader
